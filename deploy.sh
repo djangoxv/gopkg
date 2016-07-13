75914be941e3dbf00b5fc4d6a1b138fdff05754c
Jeremy Capps <djangoxv@gmail.com>
2016-07-13 23:50:37 +0000

    Added CD for artifactory service: http://52.40.15.68

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Uses curl to deploy to artifactory on successful run of test
+
+curl -udjangoxv:APEBJ2ebyFBTBsQ1 -T $GOPATH/bin/gopkg "http://52.40.15.68/artifactory/gopkg/latest/gopkg"
