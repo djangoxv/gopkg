27ca128fba024827e5c0f73406f1fd3100b806b0
Jeremy Capps <djangoxv@gmail.com>
2016-07-14 00:13:58 +0000

    finishing deployment extras

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Uses curl to deploy to artifactory on successful run of test
 
-curl -udjangoxv:APEBJ2ebyFBTBsQ1 -T $GOPATH/bin/gopkg "http://52.40.15.68/artifactory/gopkg/latest/gopkg"
+curl -udjangoxv:APEBJ2ebyFBTBsQ1 -T ./gopkg "http://52.40.15.68/artifactory/gopkg/latest/gopkg"
