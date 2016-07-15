c90e36aaed5d2724ccdeaaa50b0247fa9ebdfe4b
Ubuntu <ubuntu@ip-172-31-31-248.us-west-2.compute.internal>
2016-07-15 05:25:44 +0000

    Using CIRCLE_ARTIFACTS for local build deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 # Uses curl to deploy to artifactory on successful run of test
+deploy_user="djangoxv"
+deploy_pw="APEBJ2ebyFBTBsQ1"
+deploy_url="http://52.40.15.68/artifactory/gopkg/latest/gopkg"
 
-curl -udjangoxv:APEBJ2ebyFBTBsQ1 -T ./gopkg "http://52.40.15.68/artifactory/gopkg/latest/gopkg"
+curl -u$deploy_user:$deploy_pw -T $1 $deploy_url 
