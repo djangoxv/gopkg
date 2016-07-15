d8a9c52b3327a33ae21ebaf3d7e05dd79dce5ff4
Jeremy Capps <djangoxv@gmail.com>
2016-07-15 07:19:43 +0000

    changed password to protect deploy user

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Uses curl to deploy to artifactory on successful run of test
 deploy_user="djangoxv"
-deploy_pw="APEBJ2ebyFBTBsQ1"
+deploy_pw=$arti_pw
 deploy_url="http://52.40.15.68/artifactory/gopkg/latest/gopkg"
 
 curl -u$deploy_user:$deploy_pw -T $1 $deploy_url 
